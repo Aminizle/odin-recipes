2e08dd57f1008454a9485c39fda408326caa58a8
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2022-03-21 23:59:32 -0400

    test

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 // Select the button
-const btn = document.querySelector(".mode");
+const btn = document.querySelector('button');
 // Select the stylesheet <link>
-const theme = document.querySelector("#styles.css");
+const theme = document.querySelector("#theme-link");
 
 // Listen for a click on the button
-btn.addEventListener("click", function() {
+// btn.addEventListener("click", function() {
   // If the current URL contains "ligh-theme.css"
-  if (theme.getAttribute("href") == "light-theme.css") {
+  //if (theme.getAttribute("href") == "css/lightMode.css") {
     // ... then switch it to "dark-theme.css"
-    theme.href = "dark-theme.css";
+   // theme.href = "css/darkMode.css";
   // Otherwise...
-  } else {
+ // } else {
     // ... switch it to "light-theme.css"
-    theme.href = "light-theme.css";
-  }
+  //  theme.href = "css/lightMode.css";
+//  }
+//});
+
+// test if button works
+btn.addEventListener('click', () => {
+    alert("The button was clicked!");
 });
